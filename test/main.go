dfeb5abe0c6f35a09dd68beb99d371ed2b9574fb
Cirno <2311975835@qq.com>
2023-12-25 22:15:48 +0800

    test

--- test/main.go
+++ test/main.go
@@ -1,19 +1,49 @@
 package main
 
 import (
+	"errors"
+	"flag"
 	"fmt"
 	"log"
-	"net/http"
 	cache "simpleCache"
+	"simpleCache/etcd"
 )
 
 var db = map[string]string{
-	"Tom":  "630",
-	"Jack": "589",
-	"Sam":  "567",
+	"A":      "1",
+	"B":      "2",
+	"C":      "3",
+	"D":      "4",
+	"E":      "5",
+	"F":      "6",
+	"G":      "7",
+	"H":      "8",
+	"I":      "9",
+	"J":      "10",
+	"K":      "11",
+	"Cirno":  "123",
+	"Koish":  "421",
+	"Satori": "353",
+	"Joker":  "123",
 }
 
 func main() {
+	addr := flag.String("addr", "", "ip:port")
+	//data := flag.String("kv", " ", "db data")
+	flag.Parse()
+
+	//log.Println(*addr, *data)
+	//kvData := strings.Split(*data, " ")
+	//for _, kv := range kvData {
+	//	kvDataSlice := strings.Split(kv, ":")
+	//	db[kvDataSlice[0]] = kvDataSlice[1]
+	//}
+	//log.Println(db)
+
+	if etcd.Client == nil {
+		panic(errors.New("etcd client  is  nil!!"))
+	}
+	cache.NewHTTPPool(*addr)
 	cache.NewGroup("scores", 2<<10, cache.GetterHandler(
 		func(key string) ([]byte, error) {
 			log.Println("[SlowDB] search key", key)
@@ -22,9 +52,6 @@ func main() {
 			}
 			return nil, fmt.Errorf("%s not exist", key)
 		}))
-
-	addr := "localhost:9999"
-	peers := cache.NewHTTPPool(addr)
-	log.Println("geecache is running at", addr)
-	log.Fatal(http.ListenAndServe(addr, peers))
+	log.Println("_cache is running at", *addr)
+	cache.Start(*addr)
 }
